7da8a9d137f2e5db5bccf2e336594da56c692412
aumchuu <narumol.jeenjuhepan@gmail.com>
2022-10-10 11:35:40 +0700

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React aumchu
+          Learn React aumchuu
         </a>
       </header>
     </div>
